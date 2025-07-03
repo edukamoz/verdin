bf930d6fa4aaaf01c744dbb3a2b9d5ee835d2385
Eduardo Kamo <eduardokamoz@gmail.com>
2025-07-03 18:41:57 -0300

    Atualizado botão para excluir transação

--- frontend/src/ui.ts
+++ frontend/src/ui.ts
@@ -62,6 +62,13 @@ export const renderTransactions = (transactions: Transaction[]): void => {
       </svg>
     `;
 
+    // Ícone de lixeira em SVG
+    const deleteIcon = `
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.134-2.033-2.134H8.716c-1.123 0-2.033.954-2.033 2.134v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+      </svg>
+    `;
+
     // Agora a última célula da tabela terá os dois botões
     tr.innerHTML = `
       <td>${transaction.description}</td>
@@ -78,9 +85,8 @@ export const renderTransactions = (transactions: Transaction[]): void => {
           data-date="${transaction.date.split("T")[0]}">
           ${editIcon}
         </button>
-        <button class="delete-btn" title="Excluir" data-id="${
-          transaction.id
-        }">X</button>
+        <button class="delete-btn" title="Excluir" data-id="${transaction.id}">
+          ${deleteIcon} </button>
       </td>
     `;
     transactionsTableBody.appendChild(tr);
